c410b761a7cd44e59276afd0e60457b11f3bc5a3
felipemattosv <felipe.vanetti@gmail.com>
2023-03-20 18:38:28 -0300

    add: chakra-ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { theme } from "./styles/theme"
+import { ChakraProvider } from "@chakra-ui/react";
 
 function App() {
   return (
-    <>
-    </>
+    <ChakraProvider theme={theme}>
+      
+    </ChakraProvider>
   );
 }
 
